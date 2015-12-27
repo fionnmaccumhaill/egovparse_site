6312eae8aec39c69cf32b99cd0e56d22da906c7d
fionnmaccumhaill <edward.foley@comcast.net>
2015-12-27 12:35:21 -0500

    Select datasets for a town

--- js/egovparse.js
+++ js/egovparse.js
@@ -1,15 +1,82 @@
 function getDataIndex () {
-	// alert("In getDataIndex");
     $.getJSON("data/dataidx.json" , function(json) {
-      //  alert("getDataIndex length:" + json.length);
-    }).success(function(jsonr) {
-	//	  alert ("returning jsonr"+jsonr.length);
-		  testReturn(jsonr);
-		  return jsonr;
-});
+    }).done(function(jsonr) {
+		  idxLoaded=true;
+		  idxFile=jsonr;
+		 // $('#dmessage').append("<p> getDataIndex:"  + idxFile[2] + " </p>");
+		  return;
+    }).fail(function() {
+		  alert ("fail");
+    }).always(function() {
+		// alert("finished");
+	});
 }
 
-function testReturn(aJsonObject) {
-	// alert("length:" + aJsonObject.length)
+function getTowns () {
+    $.getJSON("data/idxtown.json" , function(json) {
+    }).done(function(jsonr1) {
+		  townLoaded=true;
+		  townFile = jsonr1
+		  loadTownsSelect(jsonr1);
+		  return;
+    }).fail(function() {
+		  alert ("fail");
+    }).always(function() {
+		
+	});
 }
-	
+
+function toTitleCase(str)
+{
+    return str.replace(/\w\S*/g, function(txt){
+		return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+
+function getDSIndex(aIndex)
+{
+	var dsIdxLength = idxFile.length;
+	for (var i = 0; i < dsIdxLength; i++) {
+		if (idxFile[i].dataId == aIndex) {
+			return idxFile[i];
+		}
+	}
+	return null;
+}
+
+function onChangeTown(aTown) {
+	$('#ddatarows').empty();
+	filterTown = aTown;
+	// showFilter(toTitleCase(filterTown));
+	var tdatasets = townFile[aTown];
+	var td_length = tdatasets.length;
+	for (var i = 0; i < td_length; i++) {
+		var dsIdx = getDSIndex(tdatasets[i]);
+		sUrl = dsIdx.url;
+		sAnchor = "<a href='" + sUrl + "'> " + dsIdx.aText + "</a>"
+		$('#ddatarows').append("<tr>");
+		// $('#ddatarows').append("<td>" + tdatasets[i] + "</td> ");
+		$('#ddatarows').append("<td>" +dsIdx.category + " </td>");
+		$('#ddatarows').append("<td>" +dsIdx.keywords + " </td>");
+	//	$('#ddatarows').append("<td>" +dsIdx.aText + " </td>");
+		$('#ddatarows').append("<td>" + sAnchor + " </td>");
+		$('#ddatarows').append("</tr>");
+	}
+}
+
+function showFilter(aTown) {
+    $('#dtown').empty();
+	$("#dtown").append("Filter: " +aTown);
+}
+
+function loadTownsSelect(aTownFile) {
+    var sortedTowns = Object.keys(aTownFile).sort();
+    var townCnt = sortedTowns.length;
+	for (var i = 0; i < townCnt; i++) {
+		 var sTown = sortedTowns[i];
+         $('#townselect')
+            .append($("<option></option>")
+            .attr("value",sTown)
+            .text(toTitleCase(sTown))); 
+	};
+	return
+}
